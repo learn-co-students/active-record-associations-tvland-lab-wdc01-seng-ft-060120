eb11c688566dd411eee49d1e34e14d1cc093a074
Tien Phan <tienphan1989@gmail.com>
2020-06-12 22:07:59 -0400

    Done.

--- app/models/character.rb
+++ app/models/character.rb
@@ -1,3 +1,18 @@
 class Character < ActiveRecord::Base
-  
+    belongs_to :actor
+    belongs_to :show
+
+    def say_that_thing_you_say 
+        "#{self.name} always says: #{self.catchphrase}"
+    end
+
+    def build_show(attributes)
+        new_show = Show.create(attributes)
+        self.show = new_show
+    end
+
+    def build_network(attributes)
+        new_network = Network.create(attributes)
+        self.show.network = new_network
+    end
 end
